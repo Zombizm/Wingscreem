1b894a6a568726d93c30624d397270a71d88249c
Zombizm <114834280+Zombizm@users.noreply.github.com>
2024-07-08 23:36:25 +0300

    some assets and menu

---
+++ res/img/Mint-Asset-Pack/Readme and Credits.txt
@@ -0,0 +1,5 @@
+If you use these assets please credit Maui Game Studio for tiles and Codemora for portraits. The backgrounds are from a different user, please check out there stuff. You are welcome to use the planets and asteriods without crediting us since they are just pixelated stock photography.
+
+Join our discord if you'd like to get ahold of Codemora or I (Joe, Maui Game Studio).
+
+https://discord.com/invite/g2ksjJx
---
+++ scripts/menu.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+
+func _on_play_btn_pressed():
+	get_tree().change_scene_to_file("res://levels/test.tscn")
+
+
+func _on_options_btn_pressed():
+	pass # Replace with function body.
+
+func _on_some_btn_pressed():
+	get_tree().quit()
