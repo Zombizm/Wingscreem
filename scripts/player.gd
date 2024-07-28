2481e09268a2502fd709acfad73517045a3c5492
Zombizm <114834280+Zombizm@users.noreply.github.com>
2024-07-28 11:16:34 +0300

    basic movements and alpha cut sprite

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,35 +1,64 @@
-extends CharacterBody2D
+extends CharacterBody3D
 
-@onready var ray = $RayCast2D
-@onready var sprite = $Sprite2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 5.0
+const SPRINTING_SPEED = 8.0
+const CROUCH_SPEED = 3.0
+const JUMP_VELOCITY = 9.5
 
+const MOUSE_SENSE = 0.4
+
+var currentSpeed = SPEED
+
+const LERP_SPEED = 10.0
+
+@onready var head = $head
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+func _ready():
+	#Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	pass
 
-func _physics_process(_delta):
+var input_dir = Input.get_vector("left", "right", "forward", "backward")
+func _input(event):
+	input_dir = Input.get_vector("left", "right", "forward", "backward")
+	
+	if not event is InputEventMouseMotion: return
+	rotate_y(-deg_to_rad(event.relative.x * MOUSE_SENSE))
+	head.rotate_x(-deg_to_rad(event.relative.y * MOUSE_SENSE))
+	head.rotation.x = clamp(head.rotation.x, deg_to_rad(-89),  deg_to_rad(89))
 
+var direction = Vector3.ZERO
+func _physics_process(delta):
+	
+	if Input.is_action_pressed("crouch") and is_on_floor():
+		currentSpeed = CROUCH_SPEED
+	elif Input.is_action_pressed("crouch"):
+		velocity.y -= 100 * delta
+	else:
+		currentSpeed = SPEED
+	
+	
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	
+	# Handle Jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	if Input.is_action_just_pressed("move_up"):
-		move(Vector2.UP)
-	elif Input.is_action_just_pressed("move_down"):
-		move(Vector2.DOWN)
-	elif Input.is_action_just_pressed("move_left"):
-		move(Vector2.LEFT)
-	elif Input.is_action_just_pressed("move_right"):
-		move(Vector2.RIGHT)
+	# var input_dir = Input.get_vector("left", "right", "forward", "backward")
+	direction = lerp(direction, (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized(), delta * LERP_SPEED)
+	if direction:
+		velocity.x = direction.x * currentSpeed
+		velocity.z = direction.z * currentSpeed
+	else:
+		velocity.x = move_toward(velocity.x, 0, currentSpeed)
+		velocity.z = move_toward(velocity.z, 0, currentSpeed)
 
 	move_and_slide()
-
-func move(direction: Vector2):
-	print(direction)
-	ray.rotation = direction.angle()
-	print(ray.collide_with_bodies)
-	if !ray.collide_with_bodies: return
-	sprite.rotation = direction.angle()
-	self.position += 64 * direction
-	
